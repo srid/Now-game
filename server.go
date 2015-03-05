6496ceaeb2303c2dbed878929064bcbd955b8cde
Sridhar Ratnakumar <srid@srid.ca>
2015-03-05 12:06:15 -0800

    Server Elm html output via :8000

--- server.go
+++ server.go
@@ -1,9 +1,20 @@
 package main
 
-import "github.com/hypebeast/go-osc/osc"
+import (
+	"net/http"
+	"github.com/hypebeast/go-osc/osc"
+)
+
+func runWebServer() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "game.html")
+	})
+	http.ListenAndServe(":8000", nil)
+}
 
 func main() {
 	addr := "0.0.0.0:5000"
+	// osc.Server starts a UDP server
 	server := &osc.Server{Addr: addr}
 
 	server.Handle("/muse/elements/experimental/concentration", func(msg *osc.Message) {
@@ -13,6 +24,8 @@ func main() {
 	server.Handle("/muse/elements/experimental/mellow", func(msg *osc.Message) {
 		osc.PrintMessage(msg)
 	})
+
+	go runWebServer()
 
 	println("Listening.", addr)
 	server.ListenAndServe()
